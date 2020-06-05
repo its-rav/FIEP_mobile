0219ba713ac0e5502d9494f2498e9d232d09acc5
Thanh1999 <t.max347056@gmail.com>
2020-06-05 23:03:39 +0700

    create basic Login screen with Google sign in

---
+++ fiep_fe_mobile/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'FunctionCLasses/colorConvert.dart';
+import 'Pages/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FIEP app",
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        primaryColor: convertColor("#ffb74d"),
+        primaryColorLight: convertColor("#ffe97d"),
+        primaryColorDark: convertColor("#c88719"),
+        secondaryHeaderColor: convertColor("#ffe082"),
+        textSelectionColor: convertColor("#000000"),
+      ),
+      home: LoginPage(),
+    );
+
+
+  }
+}
